37bbfd4d78af06d6c11a54f90903f6b0192953ab
Manuel Santos <manueljoaosantos@gmail.com>
2021-08-17 19:27:05 +0100

    Handling Errors Globally with the Built-In Middleware. Execução de: get-publisher-by-id

---
+++ my-books/Exceptions/ExceptionMiddlewareExtentions.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.Features;
+using my_books.Data.Models.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace my_books.Exceptions
+{
+    public static class ExceptionMiddlewareExtentions
+    {
+        public static void ConfigBuilderInExceptionHandler(this IApplicationBuilder app)
+        {
+            app.UseExceptionHandler(appError =>
+            {
+                appError.Run(async context => 
+                {
+                    context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+                    context.Response.ContentType = "application/json";
+
+                    var contextFeature = context.Features.Get<IExceptionHandlerFeature>();
+                    var contextRequest = context.Features.Get<IHttpRequestFeature>();
+
+                    if(contextFeature != null)
+                    {
+                        await context.Response.WriteAsync(new ErrorVM()
+                        {
+                            StatusCode = context.Response.StatusCode,
+                            Message = contextFeature.Error.Message,
+                            Path = contextRequest.Path
+                        }.ToString());
+                    }
+                });
+            });
+        }
+    }
+}
