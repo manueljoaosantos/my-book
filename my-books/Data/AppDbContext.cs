cf7a87875ba5040812a0edf14cca3d5f83c16e09
Manuel Santos <manueljoaosantos@gmail.com>
2021-08-20 18:34:55 +0100

    Adding a new Log table using EFCore migrations

--- my-books/Data/AppDbContext.cs
+++ my-books/Data/AppDbContext.cs
@@ -18,15 +18,19 @@ namespace my_books.Data
                     .WithMany(ba => ba.Book_Authors)
                     .HasForeignKey(bi => bi.BookId);
 
-        modelBuilder.Entity<Book_Author>()
+            modelBuilder.Entity<Book_Author>()
                     .HasOne(a => a.Author)
                     .WithMany(ba => ba.Book_Authors)
                     .HasForeignKey(ai => ai.AuthorId);
+
+            modelBuilder.Entity<Log>().HasKey(n => n.Id);
+
         }
 
         public DbSet<Book> Books { get; set; }
         public DbSet<Author> Authors { get; set; }
         public DbSet<Book_Author> Books_Authors { get; set; }
         public DbSet<Publisher> Publishers { get; set; }
+        public DbSet<Log> Logs { get; set; }
     }
 }
